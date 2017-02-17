42ef99fb31daea765428e638f5116f5f64ddba24
Derek Egel <derekegel@cv15359.corp.classifiedventures.com>
2017-02-17 12:08:51 -0600

    lesson 27

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,7 +3,7 @@
 
   angular
     .module("ngClassifieds")
-    .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) {
+    .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
 
       classifiedsFactory.getClassifieds().then(function(classifieds) {
         $scope.classifieds = classifieds.data;
@@ -39,11 +39,25 @@
         $scope.classified = classified;
       }
 
-      $scope.saveEdit = function () {
+      $scope.saveEdit = function() {
         $scope.editing = false;
         $scope.classified = {};
         $scope.closeSidebar();
         showToast("Edit Saved!");
+      }
+
+      $scope.deleteClassified = function(event, classified) {
+        var confirm = $mdDialog.confirm()
+          .title('Are you sure you want to delete ' + classified.title + '?')
+          .ok('Yes')
+          .cancel('No')
+          .targetEvent(event);
+        $mdDialog.show(confirm).then(function () {
+          var index = $scope.classifieds.indexOf(classified);
+          $scope.classifieds.splice(index, 1);
+        }, function () {
+
+        });
       }
 
       function showToast(message) {
