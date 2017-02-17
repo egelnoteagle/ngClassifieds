bb21db9f34a2bebf01077ea9bb5edd9f6dc7bb60
Derek Egel <derekegel@cv15359.corp.classifiedventures.com>
2017-02-17 09:53:40 -0600

    lesson 25

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,11 +3,40 @@
 
   angular
     .module("ngClassifieds")
-    .controller("classifiedsCtrl", function($scope, $http) {
+    .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) {
 
-      $http.get('data/classifieds.json').then(function(classifieds) {
+      classifiedsFactory.getClassifieds().then(function(classifieds) {
         $scope.classifieds = classifieds.data;
       });
+
+      var contact = {
+        name: "Derek Egel",
+        phone: "(773)-726-1212",
+        email: "test@test.com"
+      }
+
+      $scope.openSidebar = function() {
+        $mdSidenav('left').open();
+      }
+
+      $scope.closeSidebar = function() {
+        $mdSidenav('left').close();
+      }
+
+      $scope.saveClassified = function(classified) {
+        if (classified) {
+          classified.contact = contact;
+          $scope.classifieds.push(classified);
+          $scope.classified = {};
+          $scope.closeSidebar();
+          $mdToast.show(
+            $mdToast.simple()
+              .content("Classified Saved!")
+              .position('top, right')
+              .hideDelay(3000)
+          );
+        }
+      }
 
     });
 })();
